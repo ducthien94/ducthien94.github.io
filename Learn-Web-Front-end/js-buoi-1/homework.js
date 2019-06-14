4cced588516cb0609b38c9030791c3eba1da0bca
= <=>
2019-06-14 22:20:58 +0700

    update homework

---
+++ Learn-Web-Front-end/js-buoi-1/homework.js
@@ -0,0 +1,27 @@
+// Bài 1
+document.write('Xin chào các bạn');
+
+//Bài 2
+function sayHello(str) {
+	document.write('Xin chào' + ' ' + str);
+}
+
+//Bài 3
+function sayHelloEx(strr) {
+	document.write('Xin chào' + ' ' + '"' + strr + '"');
+}
+
+//Bài 4
+function sum(x, y) {
+	console.log(x + y);
+}
+
+//Bài 5
+function square(z) {
+  console.log(x * x);
+}
+
+//Bài 6
+function century(year) {
+  console.log(Math.round((Math.ceil(year/100) + Math.floor(year/100))/2));
+}
