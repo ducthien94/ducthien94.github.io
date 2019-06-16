724a19eba19adabfba3e30f043d2eef272220034
= <=>
2019-06-16 10:25:29 +0700

    update hw7

--- Learn-Web-Front-end/js-buoi-1-FE-b7/homework.js
+++ Learn-Web-Front-end/js-buoi-1-FE-b7/homework.js
@@ -5,23 +5,29 @@ document.write('Xin chào các bạn');
 function sayHello(str) {
 	document.write('Xin chào' + ' ' + str);
 }
+sayHello('cả lớp');
 
 //Bài 3
-function sayHelloEx(strr) {
+function sayHello2(strr) {
 	document.write('Xin chào' + ' ' + '"' + strr + '"');
 }
+sayHello2('bà con');
+
 
 //Bài 4
 function sum(x, y) {
 	console.log(x + y);
 }
+sum(3,5);
 
 //Bài 5
 function square(z) {
-  console.log(x * x);
+  console.log(z * z);
 }
+square(8);
 
 //Bài 6
 function century(year) {
   console.log(Math.round((Math.ceil(year/100) + Math.floor(year/100))/2));
 }
+century(2019);
